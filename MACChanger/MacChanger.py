90dbee6d93734f85ef58987eb77a55935ec801e9
AnviPuri <anvipuri@gmail.com>
2020-02-20 19:24:13 +0530

    Using command line variables to change MAC Address

--- MACChanger/MacChanger.py
+++ MACChanger/MacChanger.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 
 import subprocess;
+import optparse;
 
-interface=input("Enter the interface");
-new_mac=input("Enter the new MAC Address");
+parser = optparse.OptionParser();
 
-##ALTERNATE WAY
-# subprocess.call("ifconfig " + interface + " down",shell=True);
-# subprocess.call("ifconfig " + interface + " hw ether " + new_mac,shell=True);
-# subprocess.call("ifconfig " + interface + " up",shell=True);
+parser.add_option("-i","--interface",dest="interface",help="Interface to change MAC Address");
+parser.add_option("-m","--mac",dest="new_mac",help="MAC Address to be changed");
+
+options,arguments = parser.parse_args();
+
+interface=options.interface
+new_mac=options.new_mac
 
 subprocess.call(["ifconfig",interface,"down"])
 subprocess.call(["ifconfig",interface,"hw","ether",new_mac])
 subprocess.call(["ifconfig",interface,"up"])
+
+##ALTERNATE WAY FOR EXECUTING THE COMMANDS
+# subprocess.call("ifconfig " + interface + " down",shell=True);
+# subprocess.call("ifconfig " + interface + " hw ether " + new_mac,shell=True);
+# subprocess.call("ifconfig " + interface + " up",shell=True);
+
